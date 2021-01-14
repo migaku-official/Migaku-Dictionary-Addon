640c136d2f4d11d8e6ad48775e0a8046855055fb
Lucas MIA <52677245+LucasMIA@users.noreply.github.com>
2021-01-13 19:32:23 -0500

    version bump

--- src/addonSettings.py
+++ src/addonSettings.py
@@ -20,7 +20,7 @@ from .addTemplate import TemplateEditor
 from .miutils import miInfo, miAsk
 
 
-verNumber = "1.2.0"
+verNumber = "1.2.1"
 
 def attemptOpenLink(cmd):
     if cmd.startswith('openLink:'):
